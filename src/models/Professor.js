5a82dc334f9f21705756097cd087ecdcf77e85c2
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-13 18:08:43 -0300

    new: Entity/Schema Professor

---
+++ src/models/Professor.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+const Schema = new mongoose.Schema();
+const ObjectId = Schema.ObjectId;
+
+const professorSchema = new Schema({
+    id : {type : ObjectId},
+    name : {type : String},
+    surname : {type : String},
+    class: {type : String},
+    mail: {type : String}
+    
+});
+
+const professors = mongoose.model('Professor', professorSchema);
+
+export default professors;
