1c3c4350a8c45200daaf4ad1b8b81da3da433847
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-13 18:32:44 -0300

    implements professor routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import alumn from './alumnsRoutes.js';
+import professors from './professorRoutes.js';
 
 const routes = function(app) {
   app.use(
     express.json(),
-    alumn
+    alumn,
+    professors
   )  
 };
 
