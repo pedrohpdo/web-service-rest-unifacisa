15287f8c5bde2e4a8a111fa6f4bfe766ee0decc2
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-13 18:34:39 -0300

    fix .get route

--- src/routes/professorRoutes.js
+++ src/routes/professorRoutes.js
@@ -3,7 +3,7 @@ import ProfessorController from '../controllers/ProfessorsController.js';
 
 const router = express.Router();
 router.get('/prof', ProfessorController.findAll);
-router.get('prof/:id', ProfessorController.findById);
+router.get('/prof/:id', ProfessorController.findById);
 router.post('/prof', ProfessorController.insert);
 router.put('/prof/:id', ProfessorController.alter);
 router.delete('/prof/:id', ProfessorController.delete);
