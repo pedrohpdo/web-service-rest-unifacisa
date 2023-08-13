3a30e6fbcaf561d798f5532295b6f20f3f50a032
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-13 18:32:32 -0300

    fix: Schema instance

--- src/models/Professor.js
+++ src/models/Professor.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
-const Schema = new mongoose.Schema();
+const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const professorSchema = new Schema({
-    id : {type : ObjectId},
-    name : {type : String},
-    surname : {type : String},
-    class: {type : String},
-    mail: {type : String}
-    
+  id: { type: ObjectId },
+  name: { type: String },
+  surname: { type: String },
+  class: { type: String },
+  mail: { type: String },
 });
 
 const professors = mongoose.model('Professor', professorSchema);
