561c62ff6805b67ec160dbf113235b289bfde225
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-14 22:15:31 -0300

    update: implements URI variable

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-import "dotenv/config.js"
-import app from "./src/app.js";
+import 'dotenv/config.js';
+import app from './src/app.js';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, function() {
-console.log(`listening on port ${port}`);
+app.listen(port, function () {
+  console.log(`listening on port ${port}`);
 });
-
