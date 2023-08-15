21858a3074eec0a89022dfb0da2dfdf63ad52011
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-15 20:53:37 -0300

    fix: implements arrow function

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import app from './src/app.js';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, function () {
+app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
