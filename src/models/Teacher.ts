aeff24c5c0780321226080af7543b1666c2fc848
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-19 20:51:09 -0300

    feat(Teacher): implements mail validation

--- src/models/Teacher.ts
+++ src/models/Teacher.ts
@@ -8,7 +8,15 @@ const teacherSchema = new Schema({
   name: { type: String, required: true },
   surname: { type: String, required: true },
   class: { type: String },
-  mail: { type: String, required: true },
+  mail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value: string) => {
+        return value.includes('@mail.com')
+      },
+    },
+  },
 })
 
 export const Teacher = mongoose.model('Teacher', teacherSchema)
