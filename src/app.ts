6f003365ee3897df926f42c8515887001f9304d9
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-18 22:34:27 -0300

    fix(ErrorHandler): update signature

--- src/app.ts
+++ src/app.ts
@@ -15,10 +15,21 @@ conn.once('open', () => {
 
 routes(app)
 
-app.use((error, res) => {
-  if (error instanceof mongoose.Error.CastError) {
-    res.status(404).json({
-      error: `${error.message}`,
-    })
-  }
-})
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    if (err instanceof mongoose.Error.CastError) {
+      res.status(422).json({ message: `${err.message}` })
+    } else if (err instanceof mongoose.Error.ValidationError) {
+      const errors = Object.values(err.errors).map((erro) => erro.message)
+      res.status(400).json({ message: `${errors}` })
+    } else {
+      res.status(500).json({ error: `${err.name}` })
+    }
+  },
+)
