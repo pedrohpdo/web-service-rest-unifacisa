20f722c79e3455d5f05ea9a39464a8f4d48cd0b0
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-20 15:15:44 -0300

    fix(notFoundEntityErro): status code: 422

--- src/error/notFoundEntityError.ts
+++ src/error/notFoundEntityError.ts
@@ -2,6 +2,6 @@ import { ErrorResponse } from './errorResponse'
 
 export class NotFountEntityError extends ErrorResponse {
   constructor(id: string) {
-    super('Not Found Entity Error', `Cannot find Student with id: ${id}`, 404)
+    super('Not Found Entity Error', `Cannot find Student with id: ${id}`, 422)
   }
 }
